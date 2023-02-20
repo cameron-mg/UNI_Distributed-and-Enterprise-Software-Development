8159b655190c3b95353cb762948120ea182e583a
tre2-wilson <thomas11.wilson@live.uwe.ac.uk>
2023-02-20 20:13:18 +0000

    TW - Initial template set-up

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse("Hello, Django!")
+    #return HttpResponse("Hello, Django!")
+    return render(request, 'app/home.html')
+    

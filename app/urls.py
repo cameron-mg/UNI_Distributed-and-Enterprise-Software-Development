bbc09cabd49e39d32b11c493b222e04358922b1f
c2-mackaygrant <cameron2.mackay-grant@live.uwe.ac.uk>
2023-03-13 19:58:45 +0000

    CMG - Adding individual dev areas to views and urls

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,12 @@ from app import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    # CMG URLS
     path("clubrephome/", views.clubrephome, name="clubrephome"),
+    # TW URLS
     path("registerClub/", views.registerClub, name="registerClub")
+    # CR URLS
+
+    # JD URLS
+    
 ]
--- app/views.py
+++ app/views.py
@@ -3,16 +3,18 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from app.models import *
 from django.shortcuts import redirect
-from app import forms
-from app import models
+from app import forms, models
 
 
 def home(request):
     return render(request, 'app/home.html')
-    
+# CMG VIEWS
+
 def clubrephome(request):
     tn = "test name"
     return render(request, 'app/clubrephome.html', {"name": tn})
+
+# TW VIEWS
 
 def registerClub(request):
     form = forms.registerClub(request.POST or None)
@@ -26,3 +28,7 @@ def registerClub(request):
             return render(request, "app/registerClub.html", {"form": form})
     else:
         return render(request, "app/registerClub.html", {"form": form})
+    
+# CR VIEWS
+
+# JD VIEWS
